66e57c98dbe08b2dc2099c005522bf3a97320d6c
Simon Staß <simonsta95@gmail.com>
2024-07-26 14:12:52 +0200

    integration test for getAll fixed

--- src/test/java/org/spring/todorecap/service/ToDoServiceIntegrationTest.java
+++ src/test/java/org/spring/todorecap/service/ToDoServiceIntegrationTest.java
@@ -8,47 +8,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//class ToDoServiceIntegrationTest {
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Autowired
-//    ToDoRepo toDoRepo;
-//
-//    @Test
-//    public void getAllToDos() throws Exception {
-//
-//        ToDo test = new ToDo("1", ToDoStatus.OPEN, "Test1");
-//        ToDo test2 = new ToDo("2", ToDoStatus.OPEN, "Test2");
-//
-//        toDoRepo.saveAll(List.of(test, test2));
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json("""
-//                                    [
-//                                      {
-//                                            "id": "1",
-//                                            "status": "OPEN",
-//                                            "description": "Test1"
-//                                      },
-//                                      {
-//                                            "id": "2",
-//                                            "status": "OPEN",
-//                                            "description": "Test2"
-//                                      }
-//                                    ]
-//                                    """));
-//
-//    }
-//}
+@SpringBootTest
+@AutoConfigureMockMvc
+class ToDoServiceIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ToDoRepo toDoRepo;
+
+    @Test
+    public void getAllToDos() throws Exception {
+
+        ToDo test = new ToDo("1", ToDoStatus.OPEN, "Test1");
+        ToDo test2 = new ToDo("2", ToDoStatus.OPEN, "Test2");
+
+        toDoRepo.saveAll(List.of(test, test2));
+
+        mockMvc.perform(get("/api/todo"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                                    [
+                                      {
+                                            "id": "1",
+                                            "status": "OPEN",
+                                            "description": "Test1"
+                                      },
+                                      {
+                                            "id": "2",
+                                            "status": "OPEN",
+                                            "description": "Test2"
+                                      }
+                                    ]
+                                    """));
+    }
+}
